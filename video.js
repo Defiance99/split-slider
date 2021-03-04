3c8658898d7a8054d9ba30fa1a35dc23ea431cca
Defiance99 <var.alo@mail.ru>
2021-03-04 23:10:36 +0300

    fixed video

--- video.js
+++ video.js
@@ -5,18 +5,19 @@ document.addEventListener('DOMContentLoaded', () => {
     let iframe = document.querySelector('.ms-vimeo-iframe');
     let html = document.querySelector('html');
     let src = iframe.getAttribute('src');
+    console.log(html)
 
     link.addEventListener('click', () => {
         iframe.setAttribute('src', src);
         layoutBg.classList.add('ms-show');
-        html.style.overflow = 'hidden !important';
+        html.classList.add('ms-overflow-hid');
         iframeWrapper.classList.add('ms-show');
     });
 
     layoutBg.addEventListener('click', () => {
         iframe.setAttribute('src', '');
         layoutBg.classList.remove('ms-show');
-        html.style.overflow = 'unset';
+        html.classList.remove('ms-overflow-hid');
         iframeWrapper.classList.remove('ms-show');
     });
     
