a67dee220f21ad5c3a9532cd286f3fc4e2833329
Defiance99 <var.alo@mail.ru>
2021-03-04 22:53:57 +0300

    update video

--- video.js
+++ video.js
@@ -1,15 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
     let link = document.querySelector('.ms-video-link');
     let layoutBg = document.querySelector('.ms-background-layout');
-    let video = document.querySelector('.ms-video__wrapper');
+    let iframeWrapper = document.querySelector('.ms-video__wrapper');
+    let iframe = document.querySelector('.ms-vimeo-iframe');
+    let src = iframe.getAttribute('src');
 
     link.addEventListener('click', () => {
+        iframe.setAttribute('src', src);
         layoutBg.classList.add('ms-show');
-        video.classList.add('ms-show');
+        iframeWrapper.classList.add('ms-show');
     });
 
     layoutBg.addEventListener('click', () => {
+        iframe.setAttribute('src', '');
         layoutBg.classList.remove('ms-show');
-        video.classList.remove('ms-show');
+        iframeWrapper.classList.remove('ms-show');
     });
+    
 });
